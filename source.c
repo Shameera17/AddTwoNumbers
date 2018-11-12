8dd22c923c9f6814234a0662b66efb7627ebd893
Shameera Carrim <42635495+siara989@users.noreply.github.com>
2018-11-12 10:43:36 +0530

    update source.c

--- source.c
+++ source.c
@@ -2,15 +2,15 @@
 
 int main() {
 
-    int num1, num2, total;
+    double num1, num2, total;
 
     printf("Enter the first number \n");
     //input the number
-    scanf("%d", &num1);
+    scanf("%f", &num1);
     printf("Enter the second number \n");
-    scanf("%d", &num2);
+    scanf("%f", &num2);
     total = num1 + num2;
-    printf("total is %d \n", total);
+    printf("total is %f \n", total);
 
     system("pause");
     return 0;
